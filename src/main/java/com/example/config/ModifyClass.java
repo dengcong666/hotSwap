be2c2540daccf925ee96abb8b81b8a55c6e13ec7
邓聪 <dengcong@cares.sh.cn>
2021-06-11 14:33:22 +0800

    文档描述

--- src/main/java/com/example/config/ModifyClass.java
+++ src/main/java/com/example/config/ModifyClass.java
@@ -13,31 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ModifyClass {
 
-    //不要使用断点模式启动应用
-    private static HotSwapper hs;
-
-    static {
-        try {
-            hs = new HotSwapper(40000);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    private HotSwapper hs;
 
     @RequestMapping({"/changeMethod"})
-    public String changeMethod(String p1, String p2, String p3) throws Exception {
+    public String changeMethod(String className, String methodName, String logic) throws Exception {
         //需要引入 tools.jar
-        //VM options -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8910  配置JRE
+        //VM options -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=40000  配置JRE
         ClassPool classPool = ClassPool.getDefault();
         classPool.appendClassPath(new LoaderClassPath(JarLoadClass.moduleClassLoader));
-        CtClass ctClass = classPool.get(p1);
+        CtClass ctClass = classPool.get(className);
         //解冻可以多次修改
         ctClass.defrost();
-        CtMethod ctMethod = ctClass.getDeclaredMethod(p2);
+        CtMethod ctMethod = ctClass.getDeclaredMethod(methodName);
         //{ return String.valueOf(666);}
-        ctMethod.setBody(p3);
-
-        hs.reload(p1, ctClass.toBytecode());
-        return "改变了" + p1 + "." + p2 + "的执行逻辑";
+        ctMethod.setBody(logic);
+        if (hs == null) {
+            hs = new HotSwapper(40000);
+        }
+        hs.reload(className, ctClass.toBytecode());
+        return "改变了" + className + "." + methodName + "的执行逻辑: " + logic;
     }
 }
